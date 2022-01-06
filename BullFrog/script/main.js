80be5cc94df2cc7d8d01e7e709ae261d9a31058a
DeveloperThing <54551931+DeveloperThing@users.noreply.github.com>
2022-01-06 03:04:00 +0100

    Add files via upload

---
+++ BullFrog/script/main.js
@@ -0,0 +1,51 @@
+function MenuHome() {
+    window.location.href = "./index.html";
+}
+
+function MenuTwitter() {
+    window.location.href = "https://twitter.com/BullFrogDAO";
+}
+
+function MenuYoutube() {
+    window.location.href = "./frognomic.html";
+}
+
+function MenuDiscord() {
+    window.location.href = "./frognation.html";
+}
+
+function SpookySwap() {
+    window.location.href = "https://spookyswap.finance/";
+}
+
+function Dexscreener() {
+    window.location.href = "https://dexscreener.com/fantom/0xd5c210a56d249bdc3a043a73a75b8ec3e5adff22";
+}
+
+function Twitch() {
+    window.location.href = "https://www.twitch.tv/froggy";
+}
+
+function Frognomics() {
+    window.location.href = "https://www.youtube.com/sebfrog";
+}
+
+function Video1() {
+    window.location.href = "https://youtu.be/j5OQu7lK0GA";
+}
+
+function Video2() {
+    window.location.href = "https://youtu.be/oZ4Y-VqwowI";
+}
+
+function Video3() {
+    window.location.href = "https://youtu.be/vXn9iLqcqd0";
+}
+
+function Video4() {
+    window.location.href = "https://youtu.be/TfiI9O-T0X4";
+}
+
+function Video5() {
+    window.location.href = "https://youtu.be/hzmosZ63k1U";
+}
